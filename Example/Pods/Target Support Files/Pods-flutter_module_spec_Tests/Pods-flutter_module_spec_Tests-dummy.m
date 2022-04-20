0ef95993916a15fa5c526315c66c143491dea5a3
zhanghua4@tiens.com <3051942353@qq.com>
2022-04-20 09:04:30 +0800

    组件初始化

---
+++ Example/Pods/Target Support Files/Pods-flutter_module_spec_Tests/Pods-flutter_module_spec_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_flutter_module_spec_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_flutter_module_spec_Tests
+@end
---
+++ Example/Pods/Target Support Files/flutter_module_spec/flutter_module_spec-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_flutter_module_spec : NSObject
+@end
+@implementation PodsDummy_flutter_module_spec
+@end
